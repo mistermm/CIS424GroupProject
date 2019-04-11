88a09c69506ee9aa05d1e459c48e87b4cfa897b6
sneubauer1 <44908149+sneubauer1@users.noreply.github.com>
2019-04-11 12:11:47 -0500

    Update main.c

--- main.c
+++ main.c
@@ -32,10 +32,13 @@ static int parse()
 int main()
 {
    printf("Hello...Welcome to The Lone Wanderer.\n");
-   printf("You wake up from a deep slumber in a small forest clearing.\n");
-   printf("Quickly realizing nothing around you seems familiar,\nYou slowly rise up and begin dusting yourself off to take a look around...\n");
+   printf("You wake up from a deep slumber in a small forest clearing,\n");
+   printf("quickly realizing nothing around you seems familiar,\nyou stumble to your feet and begin dusting yourself off to take a look around...\n\n");
+   printf("Directly in front of you there is a dirt road that cuts between the trees.\n");
    printf("You notice a hill with a small cottage at the top to your left.\n");
    printf("Looking towards your right you notice a small stream.\n");
+   printf("(Hint: try testing out commands like: go east or go right to begin exploring)\n");
+   printf("What would you like to do?\n");
    while ( playerInput() && parse() );
  
     
