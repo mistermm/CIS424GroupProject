7a4c5b1b2c784b58ac2997ba9fbf050f71aa1d60
mistermm <48975955+mistermm@users.noreply.github.com>
2019-04-11 20:46:56 -0500

    Update locations.c

--- locations.c
+++ locations.c
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 
- public void Start (){ 
+public class Location { 
+
+        Scanner myScanner = new Scanner(System.in); /** this is for my keyword input so i can test what i do  **/
+	      Scanner enterScanner = new Scanner(System.in);
+         
+        String choice; /** this is how i pull up the keyword **/
+          
+          
+          
+          public static void main(String[] args){
+          
+              Location dublin;
+              dublin = new Location();
+              
+              dublin.Start(); 
+          }
+          
+          public void Start (){ 
           System.out.println ("Hello...Welcome to The Lone Wanderer.\n");
           System.out.println("You wake up from a deep slumber in a small forest clearing,\n");
           System.out.println("quickly realizing nothing around you seems familiar,\nyou stumble to your feet and begin dusting yourself off to take a look around...\n\n");
@@ -8,20 +26,68 @@
           System.out.println("Looking towards your right you notice a small stream.\n");
           System.out.println("(Hint: try testing out commands like: go east or go right to begin exploring!)\n");
           System.out.println("What would you like to do?\n");
+         
+          choice = myScanner.nextLine (); 
+           /** using OR for either word **/
+          if ("east".equals(choice) || "right".equals(choice) ) {
+         enterScanner.nextLine();
+          Stream();
+          }
           
+          else if ("west".equals(choice) || "left".equals(choice)) {
+           enterScanner.nextLine();
+              SmallCottage ();
+          }
           
-          choice = scan.nextLine();
+           /** this brings him to the begining of the game if types anything else, this can be changed **/
+          else { 
+               enterScanner.nextLine();
+                Start ();
+          }
           
-          If(choice=="east") {
-              
-              Stream (); }
+          }
+         public void Stream (){ 
+    	System.out.println("\n------------------------------------------------------------------\n");
+		System.out.println("You are at the small stream.");
+		System.out.println("Across the stream you hear voices. You could swim towards them"); /** the player could drown if try to swim **/
+		System.out.println("On your left there is and object"); /** some object to pick up**/
+		System.out.println("On your right"); /** something **/
+		System.out.println(" What do you want to do ?");
+                System.out.println(" (Hint: try 'Swim' or 'pick up') ");
+		System.out.println("\n------------------------------------------------------------------\n");
+    
+    
+    
+}  
+         public void SmallCottage (){ 
+             
+                System.out.println("\n------------------------------------------------------------------\n");
+		System.out.println("You are at the Small Cottage.");
+		System.out.println("****"); /** the player could drown if try to swim **/
+		System.out.println("On your left there is and object"); /** some object to pick up**/
+		System.out.println("On your right"); /** something **/
+		System.out.println(" What do you t to do ?");
+		System.out.println("\n------------------------------------------------------------------\n");
+         }
+          }
+
+   
           
-          else if (choice == "west") { 
-                  
-                  Small Cottage();
-                  }
-          else {
-                  
-                  Start (); /** comment**/ 
-                  }
-          
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
